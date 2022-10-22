401dc0e312df38a736546b950017f3e0000d443d
Selwyn Hoeks <36075931+SHoeks@users.noreply.github.com>
2022-10-22 13:39:44 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,17 @@
 #!/bin/sh -l
 #cd $1
 #mogrify -path . -auto-orient -thumbnail $2 *.*
-$1
+cd /jpg/
+for i in $(ls *.jpg); do
+  echo $i
+  w=$(identify -format "%w" $i)
+  h=$(identify -format "%h" $i)
+  if [[ $w > $h ]];then
+     convert -quality 80 -resize 400 ${1%%.*}_400.webp
+  else
+     convert -quality 80 -resize X400 $i ${1%%.*}_400.webp
+  fi
+done
+
+
+
