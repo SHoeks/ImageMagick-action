eea0996e9cfe0d4b1368fff0e42fb25cdde9dce2
Selwyn Hoeks <36075931+SHoeks@users.noreply.github.com>
2022-10-22 13:51:10 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,11 @@ for i in $(ls *.jpg); do
   echo $i
   w=$(identify -format "%w" $i)
   h=$(identify -format "%h" $i)
+  cp $i ${i%%.*}_400.jpg
   if [[ $w > $h ]];then
-     convert -quality 80 -resize 400 ${i%%.*}_400.webp
+     mogrify -format webp -quality 80 -resize 400 ${i%%.*}_400.jpg
   else
-     convert -quality 80 -resize x400 $i ${i%%.*}_400.webp
+     mogrify -format webp -quality 80 -resize x400 ${i%%.*}_400.jpg
   fi
 done
 
