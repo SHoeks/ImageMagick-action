e1628a74df657620a3f96b9716a745135bb3d685
Selwyn Hoeks <36075931+SHoeks@users.noreply.github.com>
2022-10-22 13:54:47 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,17 +2,18 @@
 #cd $1
 #mogrify -path . -auto-orient -thumbnail $2 *.*
 cd jpg/
+find . -type f ! -name '*.jpg' -delete
+
 for i in $(ls *.jpg); do
   echo $i
   w=$(identify -format "%w" $i)
   h=$(identify -format "%h" $i)
   cp $i ${i%%.*}_400.jpg
-  if [[ $w > $h ]];then
-     mogrify -format webp -quality 80 -resize 400 ${i%%.*}_400.jpg
-  else
-     mogrify -format webp -quality 80 -resize x400 ${i%%.*}_400.jpg
-  fi
+  mogrify -format webp -quality 80 -resize "^400>" ${i%%.*}_400.jpg
 done
+
+find . -type f ! -name '*.jpg' -delete
+
 
 
 
