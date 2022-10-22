a20814f9975ed9c659441d74c0614da6c6020163
Selwyn Hoeks <36075931+SHoeks@users.noreply.github.com>
2022-10-22 14:04:23 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,16 @@ find . -type f ! -name '*.jpg' -delete
 
 for i in $(ls *.jpg); do
   echo $i
+  
   w=$(identify -format "%w" $i)
   h=$(identify -format "%h" $i)
-  cp $i ${i%%.*}_400.jpg
-  convert -quality 80 -resize "^400>" ${i%%.*}_400.jpg ${i%%.*}_400.webp
+  
+  if [[ $w > $h ]];then
+      convert -quality 80 -resize 300x200\! ${i%%.*}_400.jpg ${i%%.*}_300x200.webp
+  else
+      convert -quality 80 -resize 200x300\! ${i%%.*}_400.jpg ${i%%.*}_200x300.webp
+  fi  
+  
 done
 
 mv *.webp ../webp
