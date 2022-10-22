31b3b528a1b67b7efdd5deb0561e7d1a4c52e75e
Selwyn Hoeks <36075931+SHoeks@users.noreply.github.com>
2022-10-22 13:40:22 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ for i in $(ls *.jpg); do
   if [[ $w > $h ]];then
      convert -quality 80 -resize 400 ${1%%.*}_400.webp
   else
-     convert -quality 80 -resize X400 $i ${1%%.*}_400.webp
+     convert -quality 80 -resize x400 $i ${1%%.*}_400.webp
   fi
 done
 
