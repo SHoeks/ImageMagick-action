ad868bffd30bd53bd1803d3e195f7de132fc5bb9
Selwyn Hoeks <36075931+SHoeks@users.noreply.github.com>
2023-02-18 15:31:07 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,72 +1,145 @@
 #!/bin/sh -l
-#cd $1
-#mogrify -path . -auto-orient -thumbnail $2 *.*
 
-# dirs
-jpg_dir="jpg/"
+WebsitePath=$(dirname $0)
+echo "Website path: "$WebsitePath
+cd "$WebsitePath"
+cd "images"
+
+qlty=80
 current_date=$(date +%Y_%m_%d_%H_%M)
 exp_dir="exports_"$current_date
-
-# cd to jpg dir and remove non jpg files
-cd $jpg_dir
-find . -type f ! -name '*.jpg' -delete
-for i in $(ls *.jpg); do
-    int=$RANDOM
-    printf -v c_name "%04d" $c
-    echo $i $c_name
-    mv $i "img_"$c_name"_"$(date +%Y%m%d)"_"$int".jpg"
-    let c=c+1
-    sleep 0.1
-done
-sleep 1
-
-# move psd to export dir
 mkdir $exp_dir
 mv *.jpg $exp_dir
-
-# convert files
 cd $exp_dir
-for i in $(ls *.jpg); do
-  echo $i
-  w=$(identify -format "%w" $i)
-  h=$(identify -format "%h" $i)
-  
-  if [[ $w -gt $h ]]; then
-      convert -quality 90 -resize 400 ${i%%.*}.jpg ${i%%.*}_small.webp
-      convert -quality 90 -resize 1000 ${i%%.*}.jpg ${i%%.*}_medium.webp
-      convert -quality 90 -resize 1700 ${i%%.*}.jpg ${i%%.*}_large.webp
-  else
-      convert -quality 90 -resize 400 ${i%%.*}.jpg ${i%%.*}_small.webp
-      convert -quality 90 -resize 600 ${i%%.*}.jpg ${i%%.*}_medium.webp
-      convert -quality 90 -resize 1200 ${i%%.*}.jpg ${i%%.*}_large.webp
-  fi  
-  
-done
+touch monochrom_tmp.js
 
-# move webp files
-mv *.webp ../../webp
-find . -type f ! -name '*.jpg' -delete
+echo "converting the following images for web:"
+ls *.jpg
+echo " "
+for f in *\ *; do mv "$f" "${f// /_}"; done
 
-# create data
-touch new_data.js
+# for i in $(ls *.jpg); do
+#     #mogrify -auto-level $i
+#     mogrify -contrast-stretch 0.5x0.05% "$i"
+
+#     w=$(identify -format "%w" $i)
+#     h=$(identify -format "%h" $i)
+#     if [[ $h -gt $w ]];then
+#         mogrify -rotate 90 $i
+#         w2=$h; h2=$w
+#     else
+#         w2=$w; h2=$h
+#     fi
+#     ratio=$(echo "($w2/$h2)" | bc -l)
+#     border_size_h=3
+#     border_size_b=$(echo "($border_size_h*$ratio)" | bc)
+#     new_height=$(echo "(3000/$ratio)" | bc)
+
+#     echo "resizing to: 3000x"$new_height
+#     mogrify -resize 3000x$new_height $i
+#     mogrify -bordercolor 'rgb(248,248,248)' -border $border_size_h%x$border_size_b% $i
+#     if [[ $h -gt $w ]];then
+#         mogrify -rotate -90 $i
+#     fi
+# done 
+
+
+n=$(ls *.jpg | wc -l); c=1
 for i in $(ls *.jpg); do
+
+    echo $c "/" $n ":" $i
+    echo "{" >> monochrom_tmp.js
+    echo "'id' : '${i%%.*}'," >> monochrom_tmp.js
     w=$(identify -format "%w" $i)
     h=$(identify -format "%h" $i)
 
-    # echo filename
-    filename="${i%.*}"
-    echo "$filename"
-
     if [[ $w -gt $h ]];then
-        echo "{'file' : '$filename', 'description' : 'Untitled', 'format' : 'L'}," >> new_data.js
+
+        # 2048w
+        convert -quality $qlty -resize 2048 $i tmp.webp
+        w=$(identify -format "%w" tmp.webp)
+        h=$(identify -format "%h" tmp.webp)
+        mv tmp.webp ${i%%.*}_$w"x"$h".webp"
+        echo "'file_large' : '$exp_dir"/"${i%%.*}_$w"x"$h.webp'," >> monochrom_tmp.js
+        echo "'large_width' : '$w', 'large_height' : '$h'," >> monochrom_tmp.js
+        
+        # 1536w
+        convert -quality $qlty -resize 1536 $i tmp.webp
+        w=$(identify -format "%w" tmp.webp)
+        h=$(identify -format "%h" tmp.webp)
+        mv tmp.webp ${i%%.*}_$w"x"$h".webp"
+        echo "'file_medium' : '$exp_dir"/"${i%%.*}_$w"x"$h.webp'," >> monochrom_tmp.js
+        echo "'medium_width' : '$w', 'medium_height' : '$h'," >> monochrom_tmp.js
+
+        # 960w
+        convert -quality $qlty -resize 960 $i tmp.webp
+        w=$(identify -format "%w" tmp.webp)
+        h=$(identify -format "%h" tmp.webp)
+        mv tmp.webp ${i%%.*}_$w"x"$h".webp"
+        echo "'file_small' : '$exp_dir"/"${i%%.*}_$w"x"$h.webp'," >> monochrom_tmp.js
+        echo "'small_width' : '$w', 'small_height' : '$h'," >> monochrom_tmp.js
+
+        # 768w
+        convert -quality $qlty -resize 768 $i tmp.webp
+        w=$(identify -format "%w" tmp.webp)
+        h=$(identify -format "%h" tmp.webp)
+        mv tmp.webp ${i%%.*}_$w"x"$h".webp"
+        echo "'file_tiny' : '$exp_dir"/"${i%%.*}_$w"x"$h.webp'," >> monochrom_tmp.js
+        echo "'tiny_width' : '$w', 'tiny_height' : '$h'," >> monochrom_tmp.js
+
+        echo "'description' : 'Test gallery 2'," >> monochrom_tmp.js
+        echo "'format' : 'landscape'," >> monochrom_tmp.js
+        echo "}," >> monochrom_tmp.js
+
     else
-        echo "{'file' : '$filename', 'description' : 'Untitled', 'format' : 'P'}," >> new_data.js
+
+        # 2048w
+        convert -quality $qlty -resize x2048 $i tmp.webp
+        w=$(identify -format "%w" tmp.webp)
+        h=$(identify -format "%h" tmp.webp)
+        mv tmp.webp ${i%%.*}_$w"x"$h".webp"
+        echo "'file_large' : '$exp_dir"/"${i%%.*}_$w"x"$h.webp'," >> monochrom_tmp.js
+        echo "'large_width' : '$w', 'large_height' : '$h'," >> monochrom_tmp.js
+        
+        # 1536w
+        convert -quality $qlty -resize x1536 $i tmp.webp
+        w=$(identify -format "%w" tmp.webp)
+        h=$(identify -format "%h" tmp.webp)
+        mv tmp.webp ${i%%.*}_$w"x"$h".webp"
+        echo "'file_medium' : '$exp_dir"/"${i%%.*}_$w"x"$h.webp'," >> monochrom_tmp.js
+        echo "'medium_width' : '$w', 'medium_height' : '$h'," >> monochrom_tmp.js
+
+        # 960w
+        convert -quality $qlty -resize x960 $i tmp.webp
+        w=$(identify -format "%w" tmp.webp)
+        h=$(identify -format "%h" tmp.webp)
+        mv tmp.webp ${i%%.*}_$w"x"$h".webp"
+        echo "'file_small' : '$exp_dir"/"${i%%.*}_$w"x"$h.webp'," >> monochrom_tmp.js
+        echo "'small_width' : '$w', 'small_height' : '$h'," >> monochrom_tmp.js
+
+        # 768w
+        convert -quality $qlty -resize x768 $i tmp.webp
+        w=$(identify -format "%w" tmp.webp)
+        h=$(identify -format "%h" tmp.webp)
+        mv tmp.webp ${i%%.*}_$w"x"$h".webp"
+        echo "'file_tiny' : '$exp_dir"/"${i%%.*}_$w"x"$h.webp'," >> monochrom_tmp.js
+        echo "'tiny_width' : '$w', 'tiny_height' : '$h'," >> monochrom_tmp.js
+
+        echo "'description' : 'Test gallery 2'," >> monochrom_tmp.js
+        echo "'format' : 'portrait'," >> monochrom_tmp.js
+        echo "}," >> monochrom_tmp.js
+
     fi
+
+    rm $i
+    let c=c+1 
+
 done
 
-# add data to index
-lineNum="$(grep -n "new data gets automatically inserted above" ../../index.html | head -n 1 | cut -d: -f1)"
+lineNum="$(grep -n "new data gets automatically inserted above" ../../monochrom.js | head -n 1 | cut -d: -f1)"
 let lineNum-=3
-sed -i.bak "${lineNum}r new_data.js" ../../index.html
+sed -i.bak "${lineNum}r monochrom_tmp.js" ../../monochrom.js
+rm "monochrom_tmp.js"
+cd ../../
 
 
