9b91e7a9fc95bd399bc083f133041e177db31186
Selwyn Hoeks <36075931+SHoeks@users.noreply.github.com>
2023-02-19 15:04:25 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -34,7 +34,7 @@ for i in $(ls *.jpg); do
         x=$(shuf -i 0-3000 -n1) # x=$(jot -r 1  0 3000)
         y=$(shuf -i 0-2000 -n1) # y=$(jot -r 1  0 2000)
         r=$(shuf -i 0-360 -n1) # r=$(jot -r 1  0 360)
-        s=$(shuf -i 1-7 -n1) # s=$(jot -r 1  1 7)
+        s=$(shuf -i 1-219 -n1) # s=$(jot -r 1  1 7)
         echo $x $y $r $s
         dust_sample="dust4_s"$s"_2.jpg"
         echo $dust_sample $j
